10ad9108d9fd0de3664514821829fb9407cfdc30
Max-77 <mpushp@mail.ru>
2020-12-19 12:06:12 +0300

    Fixed many forms for rating

--- src/components/rpl.js
+++ src/components/rpl.js
@@ -13,20 +13,20 @@ const list = videos.map((video) =>
     <div class="video">
     <video src={video} controls/>
     <form method="POST" action={str+name_videos[i]}>
-        <div className={"rating"+i}>
-            <input type="radio" id="star-5" name="user_rating" value="5"/>
-            <label htmlFor="star-5" title="Оценка «5»"></label>
-            <input type="radio" id="star-4" name="user_rating" value="4"/>
-            <label htmlFor="star-4" title="Оценка «4»"></label>
-            <input type="radio" id="star-3" name="user_rating" value="3"/>
-            <label htmlFor="star-3" title="Оценка «3»"></label>
-            <input type="radio" id="star-2" name="user_rating" value="2"/>
-            <label htmlFor="star-2" title="Оценка «2»"></label>
-            <input type="radio" id="star-1" name="user_rating" value="1"/>
-            <label htmlFor="star-1" title="Оценка «1»"></label>
+        <div className="rating">
+            <input type="radio" id={"star-5-"+i} name={"user_rating"} value="5"/>
+            <label htmlFor={"star-5-"+i} title="Оценка «5»"></label>
+            <input type="radio" id={"star-4-"+i} name={"user_rating"} value="4"/>
+            <label htmlFor={"star-4-"+i} title="Оценка «4»"></label>
+            <input type="radio" id={"star-3-"+i} name={"user_rating"} value="3"/>
+            <label htmlFor={"star-3-"+i} title="Оценка «3»"></label>
+            <input type="radio" id={"star-2-"+i} name={"user_rating"} value="2"/>
+            <label htmlFor={"star-2-"+i} title="Оценка «2»"></label>
+            <input type="radio" id={"star-1-"+i} name={"user_rating"} value="1"/>
+            <label htmlFor={"star-1-"+i} title="Оценка «1»"></label>
         </div>
         <div class="button">
-            <input type="Submit"/>
+            <input type="Submit" value="Rate!"/>
         </div>
     </form>
         {i++}
